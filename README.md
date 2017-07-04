f28c3aa69090d03ca1ce2a095a21f4691c4edaaf
Gautier Nilhcem <nilhcem@gmail.com>
2017-07-04 21:02:41 +0200

    Create project

---
+++ README.md
@@ -0,0 +1,20 @@
+# Matrix Keypad driver for Android Things
+
+A port of the Keypad Arduino library for Android Things.
+
+![photo][]
+
+## Download
+
+```groovy
+dependencies {
+    compile 'com.nilhcem.androidthings:driver-keypad:0.0.1'
+}
+```
+
+## Usage
+
+![schematic][]
+
+[photo]: TODO
+[schematic]: TODO
---
+++ sample/src/main/java/com/nilhcem/androidthings/driver/keypad/sample/SampleActivity.java
@@ -0,0 +1,25 @@
+package com.nilhcem.androidthings.driver.keypad.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Message;
+import android.util.Log;
+
+import java.io.IOException;
+
+public class SampleActivity extends Activity {
+
+    private static final String TAG = SampleActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
